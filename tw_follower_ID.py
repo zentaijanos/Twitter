094b8a4919311bb5ee5c5dd11d21ce9b5be0c8ea
zentaijanos <zentai.janos93@gmail.com>
2016-10-23 13:45:41 +0200

    Update tw_follower_ID.py

--- tw_follower_ID.py
+++ tw_follower_ID.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python
 
-#sajatID=773118564236685312
-#Trump=25073877
-
 from twython import Twython 
 import time
 import sys
 import string
 import simplejson
-
-
-APP_KEY = 'DZ1oGQ5oovYcBrMakOLdDH0zh'
-APP_SECRET = 'mC7kgkSzkPfeMQ04E1SOnDnftZofetQcWs5mIFsHlM4nBOw7Bq'
-OAUTH_TOKEN = '773118564236685312-0nWaPky0ZQ6Cn4FgeZ8jSTTBJ85Vpul'
-OAUTH_TOKEN_SECRET = 'U7ikfn6jfupKCvIUFWey5ke8WB2g1OS5j2RSqL2nH81cT'
-
-
 
 
 twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
@@ -34,7 +23,7 @@ next_cursor = -1
 
 def followerID(a):
 	next_cursor = -1
-	follower_id=twitter.get_followers_ids(user_id='1248382346',cursor=next_cursor)
+	follower_id=twitter.get_followers_ids(user_id='12345',cursor=next_cursor)
 	return follower_id
 	print follower_id
 	time.sleep(10)
@@ -51,7 +40,7 @@ def followerID(a):
 #f = open('ID25073877_followersID.dat', 'w')	
 f = open('probaID.dat', 'w')	
 while(next_cursor):
-	follower_id=twitter.get_followers_ids(user_id='478595317',cursor=next_cursor)
+	follower_id=twitter.get_followers_ids(user_id='12345',cursor=next_cursor)
 	#print follower_id
 	time.sleep(60)
 	#print "Next step"
